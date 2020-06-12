b528ee97488e08c2ba14f383ccc2e3144a7f0ad8
trek-ash <yashsinghgreat@gmail.com>
2020-06-13 02:20:52 +0530

    Frontend initalised with dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TaskList  from './TasksList';
 
 function App() {
+  const tasks = [{
+    title: "Beta",
+    category: "Dev",
+    description: "This is first task",
+    done: false
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }, {
+    title: "Geta",
+    category: "Dev",
+    description: "This is first task",
+    done: true
+  }]
+  const [taskList, updateTaskList] = useState(tasks)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Things To Do!
       </header>
+      <TaskList 
+          
+          tasks={taskList}
+        />
     </div>
   );
 }
---
+++ index.js
@@ -0,0 +1,36 @@
+  
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 8000;
+const bodyParser = require('body-parser')
+const path = require("path")
+
+require("dotenv").config();
+// CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, instancekey, email, Access-Control-Request-Headers, x-http-method-override");
+  res.header("Access-Control-Max-Age", '1800');
+  if (req.method === 'OPTIONS') {
+      res.end();
+  } else {
+      res.msg = {}
+      next();
+  }
+})
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json())
+
+
+app.use(express.static(path.join(__dirname, "frontend", "build")))
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
+app.listen(port, ()=>{
+    console.log("server running..")
+})
